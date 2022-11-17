b7dacc5b081af37e32d9cb1eec19f0b7df403fe0
Rafael Rios <rvrios22@gmail.com>
2022-11-16 17:36:28 -0800

    add: routing completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Layout from './pages/Layout';
+import NoPage from './pages/NoPage';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Layout />} />
+        <Route index element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<NoPage />} />
+      </Routes>
+    </>
+
   );
 }
 
---
+++ src/pages/NoPage.js
@@ -0,0 +1,5 @@
+const NoPage = () => {
+    return <h1>Err 404</h1>;
+  };
+  
+  export default NoPage;
