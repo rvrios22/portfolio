14b8c0ee412dff0e286bda9e42b611a7a368e113
Rafael Rios <rvrios22@gmail.com>
2022-12-15 14:43:46 -0800

    resolved weather page issues

--- src/pages/weather-app/api.js
+++ src/pages/weather-app/api.js
@@ -1,3 +1,11 @@
+// export const geoApiOptions = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '7311f43d0dmshb2521443f957a72p14ce1djsnc76e4c1ffabe',
+// 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+// 	}
+// };
+
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
@@ -5,6 +13,7 @@ export const geoApiOptions = {
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
+
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
 
--- src/pages/wordle-clone/components/Keyboard.js
+++ src/pages/wordle-clone/components/Keyboard.js
@@ -46,18 +46,18 @@ function Keyboard() {
         <div className='keyboard' onKeyDown={handleKeyboard}>
             <div className='line1'>
                 {keys1.map(key => {
-                    return <Key keyVal={key} disabled={disabledLetters.includes(key)} />
+                    return <Key keyVal={key} key={key} disabled={disabledLetters.includes(key)} />
                 })}
             </div>
             <div className='line2'>
                 {keys2.map(key => {
-                    return <Key keyVal={key} disabled={disabledLetters.includes(key)}/>
+                    return <Key keyVal={key}key={key}  disabled={disabledLetters.includes(key)}/>
                 })}
             </div>
             <div className='line3'>
                 <Key keyVal={'ENT'} bigKey />
                 {keys3.map(key => {
-                    return <Key keyVal={key} disabled={disabledLetters.includes(key)}/>
+                    return <Key keyVal={key} key={key} disabled={disabledLetters.includes(key)}/>
                 })}
                 <Key keyVal={'DEL'} bigKey />
 
